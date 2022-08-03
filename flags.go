dcd778c58968a6e6c0b4b95f1f79435ea88ba5f2
inphi <mlaw2501@gmail.com>
2022-08-02 21:36:44 -0400

    init

---
+++ flags.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"github.com/urfave/cli"
+)
+
+var (
+	TxRPCURLFlag = cli.StringFlag{
+		Name:  "rpc-url",
+		Usage: "Address of exuection node JSON-RPC endpoint",
+		Value: "http://127.0.0.1:8545",
+	}
+	TxBlobFileFlag = cli.StringFlag{
+		Name:     "blob-file",
+		Usage:    "Blob file data",
+		Required: true,
+	}
+	TxToFlag = cli.StringFlag{
+		Name:     "to",
+		Usage:    "tx to address",
+		Required: true,
+	}
+	TxValueFlag = cli.StringFlag{
+		Name:  "value",
+		Usage: "tx value (wei deonominated)",
+		Value: "0x0",
+	}
+	TxPrivateKeyFlag = cli.StringFlag{
+		Name:     "private-key",
+		Usage:    "tx private key",
+		Required: true,
+	}
+	TxNonceFlag = cli.Int64Flag{
+		Name:  "nonce",
+		Usage: "tx nonce",
+		Value: -1,
+	}
+	TxGasLimitFlag = cli.Uint64Flag{
+		Name:  "gas-limit",
+		Usage: "tx gas limit",
+		Value: 21000,
+	}
+	TxGasPriceFlag = cli.StringFlag{
+		Name:  "gas-price",
+		Usage: "sets the tx max_fee_per_gas",
+	}
+	TxPriorityGasPrice = cli.StringFlag{
+		Name:  "priority-gas-price",
+		Usage: "Sets the priority fee per gas",
+		Value: "2000000000",
+	}
+
+	DownloadBeaconP2PAddr = cli.StringFlag{
+		Name:     "beacon-p2p-addr",
+		Usage:    "P2P address of beacon node",
+		Required: true,
+	}
+	DownloadSlotFlag = cli.Int64Flag{
+		Name:     "slot",
+		Usage:    "Slot to download blob from",
+		Required: true,
+	}
+)
+
+var TxFlags = []cli.Flag{
+	TxRPCURLFlag,
+	TxBlobFileFlag,
+	TxToFlag,
+	TxValueFlag,
+	TxPrivateKeyFlag,
+	TxNonceFlag,
+	TxGasLimitFlag,
+	TxGasPriceFlag,
+	TxPriorityGasPrice,
+}
+
+var DownloadFlags = []cli.Flag{
+	DownloadBeaconP2PAddr,
+	DownloadSlotFlag,
+}
---
+++ main.go
@@ -0,0 +1,167 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/big"
+	"os"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/holiman/uint256"
+	"github.com/protolambda/ztyp/view"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Commands = []cli.Command{
+		{
+			Name:   "tx",
+			Usage:  "send a blob transaction",
+			Action: TxApp,
+			Flags:  TxFlags,
+		},
+		{
+			Name:   "download",
+			Usage:  "download blobs from the beacon net",
+			Action: DownloadApp,
+			Flags:  DownloadFlags,
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatalf("App failed: %v", err)
+	}
+}
+
+func TxApp(cliCtx *cli.Context) error {
+	addr := cliCtx.GlobalString(TxRPCURLFlag.Name)
+	to := common.HexToAddress(cliCtx.GlobalString(TxToFlag.Name))
+	prv := cliCtx.GlobalString(TxPrivateKeyFlag.Name)
+	file := cliCtx.GlobalString(TxBlobFileFlag.Name)
+	nonce := cliCtx.GlobalInt64(TxNonceFlag.Name)
+	value := cliCtx.GlobalString(TxValueFlag.Name)
+	gasLimit := cliCtx.GlobalUint64(TxGasLimitFlag.Name)
+	gasPrice := cliCtx.GlobalString(TxGasPriceFlag.Name)
+	priorityGasPrice := cliCtx.GlobalString(TxPriorityGasPrice.Name)
+
+	value256, err := uint256.FromHex(value)
+	if err != nil {
+		return fmt.Errorf("invalid value param: %v", err)
+	}
+
+	data, err := os.ReadFile(file)
+	if err != nil {
+		return fmt.Errorf("error reading blob file: %v", err)
+	}
+
+	chainId := big.NewInt(1331)
+	signer := types.NewDankSigner(chainId)
+
+	ctx := context.Background()
+	client, err := ethclient.DialContext(ctx, addr)
+	if err != nil {
+		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+	}
+
+	key, err := crypto.HexToECDSA(prv)
+	if err != nil {
+		return fmt.Errorf("%w: invalid private key", err)
+	}
+
+	if nonce == -1 {
+		pendingNonce, err := client.PendingNonceAt(ctx, crypto.PubkeyToAddress(key.PublicKey))
+		if err != nil {
+			log.Fatalf("Error getting nonce: %v", err)
+		}
+		nonce = int64(pendingNonce)
+	}
+
+	var gasPrice256 *uint256.Int
+	if gasPrice == "" {
+		val, err := client.SuggestGasPrice(ctx)
+		if err != nil {
+			log.Fatalf("Error getting suggested gas price: %v", err)
+		}
+		var ok bool
+		gasPrice256, ok = uint256.FromBig(val)
+		if !ok {
+			log.Fatalf("gas price is too high!")
+		}
+	} else {
+		gasPrice256, err = decodeUint25Sting(gasPrice)
+		if err != nil {
+			return fmt.Errorf("%w: invalid gas price", err)
+		}
+	}
+
+	priorityGasPrice256 := gasPrice256
+	if priorityGasPrice != "" {
+		priorityGasPrice256, err = decodeUint25Sting(priorityGasPrice)
+		if err != nil {
+			return fmt.Errorf("%w: invalid priority gas price", err)
+		}
+	}
+
+	blobs := EncodeBlobs(data)
+	commitments, versionedHashes, aggregatedProof, err := blobs.ComputeCommitmentsAndAggregatedProof()
+	if err != nil {
+		log.Fatalf("failed to compute commitments: %v", err)
+	}
+
+	txData := types.SignedBlobTx{
+		Message: types.BlobTxMessage{
+			ChainID:             view.Uint256View(*uint256.NewInt(chainId.Uint64())),
+			Nonce:               view.Uint64View(nonce),
+			Gas:                 view.Uint64View(gasLimit),
+			GasFeeCap:           view.Uint256View(*gasPrice256),
+			GasTipCap:           view.Uint256View(*priorityGasPrice256),
+			Value:               view.Uint256View(*value256),
+			To:                  types.AddressOptionalSSZ{Address: (*types.AddressSSZ)(&to)},
+			BlobVersionedHashes: versionedHashes,
+		},
+	}
+
+	wrapData := types.BlobTxWrapData{
+		BlobKzgs:           commitments,
+		Blobs:              blobs,
+		KzgAggregatedProof: aggregatedProof,
+	}
+	tx := types.NewTx(&txData, types.WithTxWrapData(&wrapData))
+	tx, err = types.SignTx(tx, signer, key)
+	if err != nil {
+		log.Fatalf("Error signing tx: %v", err)
+	}
+
+	err = client.SendTransaction(ctx, tx)
+	if err != nil {
+		return fmt.Errorf("%w: unable to send transaction", err)
+	}
+
+	log.Printf("Transaction submitted. nonce=%d hash=%v", nonce, tx.Hash())
+
+	return nil
+}
+
+func decodeUint25Sting(hexOrDecimal string) (*uint256.Int, error) {
+	var base = 10
+	if strings.HasPrefix(hexOrDecimal, "0x") {
+		base = 16
+	}
+	b, ok := new(big.Int).SetString(hexOrDecimal, base)
+	if !ok {
+		return nil, fmt.Errorf("invalid value")
+	}
+	val256, ok := uint256.FromBig(b)
+	if !ok {
+		return nil, fmt.Errorf("value is too big")
+	}
+	return val256, nil
+}
